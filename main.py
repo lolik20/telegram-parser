0ba2d52de124430bc9b7aec16af9017301c0288b
Максим Максим <mailsteama230@mail.ru>
2023-01-04 14:35:07 +0300

    update

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ settingStream = open ("./settings.txt")
 settings = settingStream.read().split(';')
 api_id = int(settings[0])
 api_hash = str(settings[1])
-host = "217.29.63.202" # a valid host
-port = 10745  # a valid port
-proxy = (socks.SOCKS5, host, port)
-client = TelegramClient('session_name', api_id, api_hash,proxy=proxy)
+# host = "217.29.63.202" # a valid host
+# port = 10745  # a valid port
+# proxy = (socks.SOCKS5, host, port)
+client = TelegramClient('session_name', api_id, api_hash)
 
 
 wordsStream = open("./words.txt",encoding = 'utf-8')
 words = wordsStream.read().split(';')
 wordsStream.close()
 client.start()
-print(words)
+print('started!')
 
 @client.on(events.NewMessage(client.get_dialogs()))
 async def handler(event):
@@ -24,11 +24,10 @@ async def handler(event):
     readStream = open("./accounts.txt","r")
     accounts = readStream.read()
     readStream.close()
-    print(event.raw_text)
     if str(sender.id) not in accounts:
         for word in words:
             if word in event.raw_text.lower():
-                print('\033[92mнайдено ключевое слово\033[0m')
+                print('\033[92msend message\033[0m')
                 writeStream = open("./accounts.txt","w")
                 writeStream.write(f'{accounts}{sender.id};')
                 writeStream.close()
